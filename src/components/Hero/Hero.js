eafc529cb5fd191473dbad0df7d558d1ffc2c703
giggling-ginger <giggling.ginger1058@gmail.com>
2022-08-31 18:20:11 +0800

    changed personal intro

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,8 +16,12 @@ const Hero = (props) => (
           Mia's Personal Portfolio
         </SectionTitle>
         <SectionText>
-          Hi! I am Mia. I am looking for a web3 junior developer job. I'm really
-          looking forward to build the Ownership Economy with the community.
+          Hi! I am Mia. I am a community-taught full stack developer with
+          experience in React, Next.js, Node.js, Solidity, Hardhat, GraphQL. I
+          love what I do but I am seeking more professional and personal
+          satisfaction. I can't wait to scale up my skillset and contribute on
+          the job. I am a self-motivated and self-managing fast learner and I
+          would thrive in an asynchronous environment.
         </SectionText>
       </LeftSection>
     </Section>
